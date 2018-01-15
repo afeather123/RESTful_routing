60c6c7eb04990a0d54f652658f662c9356dfd168
Alec <afeather@asu.edu>
2018-01-15 10:50:38 -0700

    Add styling, show and new route

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.get("/",function(req,res){
   res.redirect("/blogs");
 })
 
+//INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({},function(err, blogs){
       if(err){
@@ -40,5 +41,32 @@ app.get("/blogs", function(req, res){
         res.render("index",{blogs:blogs});
       }
     });
+});
+//NEW ROUTE
+app.get("/blogs/new", function(req,res){
+  res.render("new");
+});
 
+//CREATE ROUTE
+app.post("/blogs", function(req,res){
+  //create blog
+  Blog.create(req.body.blog,function(err,newBlog){
+    if(err){
+      res.render("new");
+    } else {
+      //then, redirect to the INDEX
+      res.redirect("/blogs");
+    }
+  });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req,res){
+    Blog.findById(req.params.id, function(err,foundBlog){
+      if(err){
+        res.redirect("/blogs");
+      } else {
+        res.render("show",{blog: foundBlog});
+      }
+    })
 });
